aa69bc93546eb30d5b1e8542a12b54cc75198f8b
Grega Dvoršak <gdvorsak97@gmail.com>
2021-05-21 16:38:45 +0200

    Attempt to update main

--- README.md
+++ README.md
@@ -14,8 +14,13 @@ Approximately 800 chat messages to be classified into predefined classes
 The database also contains additional information such as message senders, group names, timestamps of messages etc.
 
 ## How to Run
-This section will be expanded when we implement a number of working classification methods
-and are able to generate results.
+
+The main two files to run the classifiers are main.py for the traditional approaches and main_neural.py for the BERT transformer model. The easiest way to ensure to have all of the libraries installed is to check the requirements.txt file in the project. To run the main.py file you need to download the pre-trained word2vec embedding from the following link:
+https://zenodo.org/record/3237458#.YHwg75MzadY
+The file you need is in the archive glove.twitter.27B.200d.txt.gz; you copy glove.twitter.27B.200d.txt from the archive into the code folder and the classifier should be ready to run.
+For the deep learning method, you can adjust some settings in the main_neural.py. We propose setting the number of epochs to at least 10 as the network usually performs better if that is done. The network was trained on an NVIDIA GeForce GTX 1060 graphics card. The number of epochs can be changed on line 98 in the main_neural.py file. Make sure the weights file weights_at_finish.h5 is in the models directory.
+
+We attempted to implement a custom network in the main_neural_custom.py file but the development was discontinued.
 
 ## The Report
 A continuously updated report for the project is available in the root directory with the name "report.pdf". 
---
+++ code/main_neural.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn import preprocessing
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
+from tqdm import tqdm
+from transformers import BertTokenizer, TFDistilBertForSequenceClassification
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data/data_mapbook.csv', quotechar="$", header=0, names=["id", "class", "text"], usecols=[1, 2])
+
+# Transform positive/negative values to 1/0s
+label_encoder = preprocessing.LabelEncoder()
+data['class'] = label_encoder.fit_transform(data['class'])
+# data.head()
+X = (np.array(data['text']))
+y = (np.array(data['class']))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5)
+print("Train dataset shape: {0}, \nTest dataset shape: {1} \nValidation dataset shape: {2}".format(
+    X_train.shape, X_test.shape, X_val.shape))
+
+bert_model = TFDistilBertForSequenceClassification.from_pretrained("sampathkethineedi/industry-classification")
+bert_tokenizer = BertTokenizer.from_pretrained("sampathkethineedi/industry-classification")
+
+pad_token = 0
+pad_token_segment_id = 0
+max_length = 256
+
+
+def plot_loss(history, label):
+    # Use a log scale on y-axis to show the wide range of values.
+    plt.semilogy(history.epoch, history.history['loss'],
+                 color='b', label='Train ' + label)
+    plt.semilogy(history.epoch, history.history['val_loss'],
+                 color='b', label='Val ' + label,
+                 linestyle="--")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show()
+
+
+def convert_to_input(text):
+    input_ids, attention_masks, token_type_ids = [], [], []
+
+    for x in tqdm(text, position=0, leave=True):
+        inputs = bert_tokenizer.encode_plus(x, add_special_tokens=True, max_length=max_length)
+
+        i, t = inputs["input_ids"], inputs["token_type_ids"]
+        m = [1] * len(i)
+
+        padding_length = max_length - len(i)
+
+        i = i + ([pad_token] * padding_length)
+        m = m + ([0] * padding_length)
+        t = t + ([pad_token_segment_id] * padding_length)
+
+        input_ids.append(i)
+        attention_masks.append(m)
+        token_type_ids.append(t)
+
+    return [np.asarray(input_ids), np.asarray(attention_masks), np.asarray(token_type_ids)]
+
+
+X_test = convert_to_input(X_test)
+X_train = convert_to_input(X_train)
+X_val = convert_to_input(X_val)
+
+
+def to_features(input_ids, attention_masks, token_type_ids, y):
+    return {"input_ids": input_ids, "attention_mask": attention_masks, "token_type_ids": token_type_ids}, y
+
+
+train_ds = tf.data.Dataset.from_tensor_slices((X_train[0], X_train[1], X_train[2], y_train)).map(to_features) \
+    .shuffle(100).batch(12).repeat(5)
+val_ds = tf.data.Dataset.from_tensor_slices((X_val[0], X_val[1], X_val[2], y_val)).map(to_features).batch(12)
+test_ds = tf.data.Dataset.from_tensor_slices((X_test[0], X_test[1], X_test[2], y_test)).map(to_features) \
+    .batch(12)
+
+# learning parameters
+optimizer = tf.keras.optimizers.Adam(0.00000001)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+
+file_path = "models/weights_best.h5"
+
+reduce_on_plateau = ReduceLROnPlateau(factor=0.1, patience=10)
+checkpoint = ModelCheckpoint(file_path, verbose=1, save_best_only=True)
+
+callbacks_list = [checkpoint, reduce_on_plateau]
+
+bert_model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+
+bert_model.load_weights("models/weights_at_finish.h5")
+bert_history = bert_model.fit(train_ds, epochs=10, validation_data=val_ds, callbacks=callbacks_list)
+plot_loss(bert_history, 'loss')
+
+bert_model.save_weights("models/weights_at_finish.h5")
+
+# results
+results_true = test_ds.unbatch()
+results_true = np.asarray([element[1].numpy() for element in results_true])
+print(results_true)
+
+results = bert_model.predict(test_ds)
+print(f"Model predictions:\n {results.logits}")
+
+results_predicted = np.argmax(results.logits, axis=1)
+
+print(f"F1 score: {f1_score(results_true, results_predicted, average='weighted')}")
+print(f"Accuracy score: {accuracy_score(results_true, results_predicted)}")
+print(f"Precision: {precision_score(results_true, results_predicted, average='weighted')}")
+print(f"Recall: {recall_score(results_true, results_predicted, average='weighted')}")
+
+# save model
+bert_model.save_pretrained('models/bert_pretrained.h5')
